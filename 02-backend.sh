c7c2fa437698321e267ec88deef310b79e9d3d2a
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 18:23:02 +0530

    initial commit

--- 02-backend.sh
+++ 02-backend.sh
@@ -14,6 +14,8 @@ N="\e[0m"
 
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S" )
 
+SCRIPT_DIR=$PWD
+
 USER_ID=$( id -u )
 
 if [ "$USER_ID" -ne 0 ]; then
@@ -33,4 +35,44 @@ VALIDATE(){
 dnf module disable nodejs -y &>> $LOG_FILE
 dnf module enable nodejs:20 -y &>> $LOG_FILE
 dnf install nodejs -y &>> $LOG_FILE
+
+id expense &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "expense system user" expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense system user"
+else
+    echo -e "$R User Already present with this name $N ... $Y SKIPPING $N"
+fi
+
+rm -rf /app
+VALIDATE $? "Removing Existing app/code"
+
+rm -rf /tmp/expense-backend.zip
+VALIDATE $? "Removing Exisiting expense-backend"
+
+mkdir -p /app &>> $LOG_FILE
+VALIDATE $? "Creating app folder for code"
+
+curl -o /tmp/expense-backend.zip curl -o /tmp/backend.tar.gz https://raw.githubusercontent.com/daws-90s/expense-documentation/refs/heads/main/artifacts/expense-backend-v3.tar.gz  &>>$LOG_FILE
+cd /app
+unzip /tmp/expense-backend.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping the expense-backend code"
+
+npm install &>> $LOG_FILE
+VALIDATE $? "Installing npm dependencies"
+
+cp $SCRIPT_DIR/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Creating systemctl service"
+
+
+dnf install mysql -y &>> $LOG_FILE
+VALIDATE $? "Installing mysql client"
+
+mysql -h mysql.learndevopskills.shop -u root -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "Loading data ... "
+
+systemctl daemon-reload &>> $LOG_FILE
+systemctl enable backend &>> $LOG_FILE
+systemctl restart backend &>> $LOG_FILE
+VALIDATE $? "Enabling and restaring the backend "
 
