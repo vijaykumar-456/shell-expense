fcdc74f27c8cd0728b5d3e5677adb77c06e81e1d
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 17:47:39 +0530

    initial commit

--- expense-instances.sh
+++ expense-instances.sh
@@ -36,7 +36,7 @@ do
             INSTANCE_ID=$( aws ec2 run-instances \
             --image-id $AMI_ID \
             --instance-type t3.micro \
-            --security-groups "expense-common" "expense-$instance" \
+            --security-groups "roboshop-common" "expense-$instance" \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=expense-$instance}]" \
             --query 'Instances[0].InstanceId' \
             --output text
